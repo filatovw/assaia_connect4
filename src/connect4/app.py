e1cc7e8648e693c6e0976d925e6d2a4e75fb78f8
Vadim Filatov <filatovw.dev@gmail.com>
2025-04-19 12:59:19 +0200

    ui class added

--- src/connect4/app.py
+++ src/connect4/app.py
@@ -1,12 +1,70 @@
+import typing as t
 import logging
 from connect4 import models
 
 
 class App:
-    def __init__(self, logger: logging.Logger, config: models.Config) -> None:
+    def __init__(
+        self,
+        logger: logging.Logger,
+        config: models.Config,
+        input_stream: t.TextIO,
+        output_stream: t.TextIO,
+    ) -> None:
         self._logger = logger
         self._config = config
+        self._ui = UI(logger, input_stream, output_stream)
 
     def run(self):
+        self._ui.show_intro()
         while True:
-            pass
+            player_id = 1
+            self._ui.print_active_player(player_id)
+            value = self._ui.read_line()
+            self._logger.info("VAL: %s", value)
+            if value == "":
+                self._ui.print_wrong_input("Value cannot be empty")
+                value = self._ui.read_line()
+
+            state = [[1, 0, 0], [0, 1, 1]]
+            self._ui.print_state(state)
+
+            self._ui.print_winner(player_id)
+
+
+class UI:
+    def __init__(
+        self, logger: logging.Logger, input_stream: t.TextIO, output_stream: t.TextIO
+    ) -> None:
+        self._logger = logger
+        self._sin = input_stream
+        self._sout = output_stream
+
+    def _write(self, message: str) -> None:
+        self._sout.write(message + "\n")
+
+    def _read_user_prompt(self) -> str:
+        self._sout.write("-->\n")
+        value = self._sin.readline()
+        return value.replace("\n", " ").strip(" ")
+
+    def show_intro(self) -> None:
+        self._write("Hello, Gamers!")
+
+    def print_active_player(self, player_id: int) -> None:
+        self._write(f"Player {player_id}")
+
+    def read_line(self) -> str:
+        return self._read_user_prompt()
+
+    def print_state(self, state: t.Any):
+        self._write(str(state))
+
+    def print_winner(self, player_id: int) -> None:
+        self._write(f"Winner is Player {player_id}")
+
+    def print_wrong_input(self, message: str) -> None:
+        self._write(f"Incorrect input: {message}")
+
+    def print_error(self, message: str) -> None:
+        self._write(f"Error: {message}")
--- src/connect4/entrypoints/cli.py
+++ src/connect4/entrypoints/cli.py
@@ -1,3 +1,4 @@
+import sys
 from connect4.config import get_config
 from connect4.logger import get_logger
 from connect4.app import App
@@ -6,8 +7,11 @@ from connect4.app import App
 def main():
     config = get_config()
     logger = get_logger("CONNECT4", config.app.log_level)
-    app = App(logger, config)
-    app.run()
+    app = App(logger, config, sys.stdin, sys.stdout)
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        logger.info("Game was interrupted by the User")
 
 
 if __name__ == "__main__":
